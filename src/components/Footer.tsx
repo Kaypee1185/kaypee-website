69940588d9b6fde197d2995208d6c2e325a9c3e0
Kaypee1185 <maduyeboah@gmail.com>
2026-03-06 08:24:11 +0000

    Delete src directory

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer(){
-  return (
-    <div className="container">
-      <div className="footer">
-        © {new Date().getFullYear()} Kaypee (Samic). Support WhatsApp: 0546183019
-      </div>
-    </div>
-  );
-}
